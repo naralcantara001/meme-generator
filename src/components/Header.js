a0e9d58610b4f0826fcaa80fbac2806d05705b84
naralcantara001.nar <naralcantara001@gmail.com>
2022-07-28 08:30:10 +0800

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <div>
+            <img src='images/troll-face.png' alt='images'/>
+            <p>Meme Generator</p>
+        </div>
+    </header>
+  )
+}
